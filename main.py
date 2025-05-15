a71495ec4163b5a2a2144e1a3e6d1145c5dbffc9
cmfitzla <cmfitzla@gmail.com>
2025-05-15 10:25:42 -0400

    B: add player triangle to the screen

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
-
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,8 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-	
+        dt = clock.tick(60) / 1000 # Convert milliseconds to seconds
+    
 
 
 if __name__ == "__main__":
